4a17adeab6008bebd79375f935c9343ec1e167c7
mithsin <hsinbluemoon@gmail.com>
2018-08-12 06:28:44 -0400

    abc

--- server.js
+++ server.js
@@ -1,89 +1,16 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
-
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Star Wars Characters (DATA)
-// =============================================================
-var friends = [
-  {
-    friendName: "yoda",
-    name: "Yoda",
-    gender: "Jedi Master",
-    age: 900,
-    professional: 2000
-  },
-  {
-    friendName: "yoda",
-    name: "Yoda",
-    gender: "Jedi Master",
-    age: 900,
-    professional: 2000
-  },
-  {
-    friendName: "yoda",
-    name: "Yoda",
-    gender: "Jedi Master",
-    age: 900,
-    professional: 2000
-  }
-];
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-// Routes
-// =============================================================
-
-app.get('/',function(req, res){
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-    });
-app.get('/survey', function(req,res){
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-    });
-//get all info
-app.get('/api/friends', function(req, res){
-    res.json(friends);
-    });
-//search specific friend
-app.get('/api/:friends?', function(req, res){
-    var chosen = req.params.friends;
-    
-    if(chosen){
-        console.log(chosen);
-    
-        for(var i = 0 ; i < friends.length; i++){
-            if(chosen === friends[i].friendName){
-                return res.json(friends[i]);
-            }                                   
-        }
-        return res.json(false);
-     }
-    return res.json(friends);
-   });
-
-// Create New friend - takes in JSON input
-  app.post("/api/friends", function(req, res) {
-
-  var newfriend = req.body;
-  newfriend.friendName = newfriend.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newfriend);
-
-  characters.push(newfriend);
-
-  res.json(newfriend);
-});
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
